66c1fe5469600aa4d3a9985ad0b5aba99248ed93
Esteban1901 <esteban.reyesl@usm.cl>
2022-05-23 01:07:02 -0400

    Create request

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<AllProducts />} />
+        <Route path="/products/:id"/>
         <Route path="/auth" element={<Auth />} />
       </Routes>
     </div>
