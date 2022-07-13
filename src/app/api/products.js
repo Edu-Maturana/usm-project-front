f2e2ae1f81b5bd4f7906cde20705d1498673a599
Eduardo Maturana Cáceres <edu@MacBook-Air-de-Eduardo.local>
2022-07-13 01:26:49 -0400

    build table and allow editing and deleting

--- src/app/api/products.js
+++ src/app/api/products.js
@@ -1,13 +1,32 @@
 import axios from "axios";
 
-const apiUrl = 'http://localhost:8080/api/v1';
+const apiUrl = "http://localhost:8080/api/v1";
 
 export const getProducts = async (sort) => {
-    const response = await axios.get(`${apiUrl}/products?priceSort=${sort}`);
-    return response.data;
+  const response = await axios.get(`${apiUrl}/products?priceSort=${sort}`);
+  return response.data;
 };
 
 export const getProduct = async (id) => {
-    const response = await axios.get(`${apiUrl}/products/${id}`);
-    return response.data;
-}
+  const response = await axios.get(`${apiUrl}/products/${id}`);
+  return response.data;
+};
+
+export const createProduct = async (product) => {
+  const response = await axios.post(`${apiUrl}/products`, product);
+  return response.data;
+};
+
+export const updateProduct = async (product) => {
+  const response = await axios.put(`${apiUrl}/products/${product.ID}`, {
+    ...product,
+    stock: Number(product.stock),
+    price: Number(product.price),
+  });
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  const response = await axios.delete(`${apiUrl}/products/${id}`);
+  return response.data;
+};
