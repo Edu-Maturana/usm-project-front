449e47fc9031895d6d15dd2ef3f3a2f3f8a7f5c4
Eduardo Maturana Cáceres <edu@MacBook-Air-de-Eduardo.local>
2022-07-17 22:04:24 -0400

    fix login form

--- src/app/components/LoginForm/LoginForm.js
+++ src/app/components/LoginForm/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Formik, Form } from "formik";
+import { useFormik } from "formik";
 import { toast } from "react-toastify";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
@@ -12,58 +12,76 @@ import "./LoginForm.css";
 export default function Login() {
   const [loading, setLoading] = useState(false);
   const { login } = useAuth();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = "El email es requerido";
+      }
+
+      if (!values.password) {
+        errors.password = "La contraseña es requerida";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      setLoading(true);
+      LogIn(values)
+        .then((response) => {
+          login(response.token);
+          toast.success("Bienvenido(a)!");
+          setTimeout(() => {
+            window.location = "/dashboard";
+          }, 1000);
+        })
+        .catch((error) => {
+          toast.error("Credenciales inválidas");
+        });
+    },
+  });
+
   return (
     <div className="Login">
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values, { setSubmitting }) => {
-          LogIn({
-            email: values.email,
-            password: values.password,
-          })
-            .then((res) => {
-              setLoading(true);
-              login(res.token);
-              setTimeout(() => {
-                window.location.href = "/dashboard";
-              }, 1000);
-              setSubmitting(false);
-            })
-            .catch((err) => {
-              toast.error("Nombre de usuario o contraseña incorrectos");
-              setSubmitting(false);
-            });
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="login-form">
-            <h2 className="title">Iniciar sesión</h2>
-            <div className="form-group">
-              <label className="Label">Email</label>
-              <InputText type="email" name="email" className="Input" />
-            </div>
-            <div className="form-group">
-              <label className="Label">Contraseña</label>
-              <InputText type="password" name="password" className="Input" />
-            </div>
-            <Button
-              type="submit"
-              loading={loading}
-              className="submit-form"
-              disabled={isSubmitting}
-            >
-              {loading ? (
-                <i
-                  className="pi pi-spin pi-spinner"
-                  style={{ fontSize: "1.5em" }}
-                ></i>
-              ) : (
-                "Ingresar"
-              )}
-            </Button>
-          </Form>
-        )}
-      </Formik>
+      <form className="login-form" onSubmit={formik.handleSubmit}>
+        <h2 className="title">Iniciar sesión</h2>
+        <div className="form-group">
+          <label className="Label">Email</label>
+          <InputText
+            type="email"
+            name="email"
+            className="Input"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label className="Label">Contraseña</label>
+          <InputText
+            type="password"
+            name="password"
+            className="Input"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <Button type="submit" className="submit-form">
+          {loading ? (
+            <i
+              className="pi pi-spin pi-spinner"
+              style={{ fontSize: "1em" }}
+            ></i>
+          ) : (
+            "Ingresar"
+          )}
+        </Button>
+      </form>
     </div>
   );
 }
