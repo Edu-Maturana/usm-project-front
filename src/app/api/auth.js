54ea2e7afedfc73b3fe1fd652b2ce8c6853f368f
Eduardo Maturana Cáceres <edu@MacBook-Air-de-Eduardo.local>
2022-07-17 21:37:27 -0400

    fix endpoint

--- src/app/api/auth.js
+++ src/app/api/auth.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { config } from "../../config";
 
 export const LogIn = async (user) => {
-  const response = await axios.post(config.apiUrl, user);
+  const response = await axios.post(`${config.apiUrl}/auth/login`, user);
   return response.data;
 };
